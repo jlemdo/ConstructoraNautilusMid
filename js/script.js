f17b65720caa0739edbc830acb563e3cca31ea01
emdo <61525981+jlemdo@users.noreply.github.com>
2022-04-24 15:19:56 -0500

    LCP

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.preloader').length){
-			$('.preloader').delay(500).fadeOut(500);
+			$('.preloader').delay(300).fadeOut(500);
 		}
 	}
 	
